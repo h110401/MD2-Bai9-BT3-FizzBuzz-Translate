e44d2c7ea227bee13724c80c016b832cd7b36373
h110401 <hung110401@gmail.com>
2022-08-02 15:09:31 +0700

    bt3

---
+++ test/FizzBuzzTranslateTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTranslateTest {
+    @Test
+    public void translate0() {
+        int number = 0;
+        String expected = "Out of range";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate100() {
+        int number = 100;
+        String expected = "Out of range";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate3() {
+        int number = 3;
+        String expected = "Fizz";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate5() {
+        int number = 5;
+        String expected = "Buzz";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate15() {
+        int number = 15;
+        String expected = "FizzBuzz";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate13() {
+        int number = 13;
+        String expected = "Fizz";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate25() {
+        int number = 25;
+        String expected = "Buzz";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void translate14() {
+        int number = 14;
+        String expected = "Fourteen";
+        String result = FizzBuzzTranslate.translate(number);
+
+        assertEquals(expected, result);
+    }
+}
